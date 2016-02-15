4c355aef917dbfd2216bfa3969a1581632ad5822
pmarecki <pmarecki.pro@gmail.com>
2016-02-15 15:57:46 +0100

    Stub of StorageRepo (storage of products)

--- src/main/java/tdd/vendingMachine/Domain/StorageRepo.java
+++ src/main/java/tdd/vendingMachine/Domain/StorageRepo.java
@@ -1,7 +1,74 @@
 package tdd.vendingMachine.Domain;
 
+import tdd.vendingMachine.Transaction.MachineException;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Repository responsible for management of the physical storage of products.
  */
 public class StorageRepo {
+    Integer[] pidAtShelf;
+    Integer[] countAtShelf;
+    List<String> internalLog;   //registers physical actions of the storage
+    final int nShelves;
+    final int maxProducts;
+
+    public StorageRepo(int nShelves, int maxProducts) {
+        this.nShelves = nShelves;
+        this.maxProducts = maxProducts;
+        pidAtShelf = new Integer[nShelves];
+        countAtShelf = new Integer[nShelves];
+        for (int i = 0; i < nShelves; i++) {
+            pidAtShelf[i] = null;
+            countAtShelf[i] = 0;
+        }
+        internalLog = new ArrayList<>();
+    }
+
+    public Integer getPidAtShelf(int shelf) {
+        return null;
+    }
+
+    public Integer getCountAtShelf(int shelf) {
+        return null;
+    }
+
+    /**
+     * @param shelf: number of shelf from which the product should be served
+     * @throws MachineException if no products are present at this shelf
+     *        (or other not yet specified condition prevents discharge of the product)
+     */
+    public void serveProduct(int shelf) throws MachineException {
+
+    }
+
+    /**
+     * Adds products to selected shelf; implicitly these are the same type of products.
+     *
+     * @param shelf selected shelf
+     * @param newProductCount (can be negative)
+     * @throws RuntimeException when count at `shelf` becomes invalid
+     */
+    public void addProductsToShelf(int shelf, int newProductCount) throws RuntimeException {
+
+    }
+
+    /**
+     * @param shelf selected shelf
+     * @param productid (will not be checked for validity in ProductRepo)
+     * @param count (must be valid; else RuntimeException thrown)
+     * @throws RuntimeException
+     */
+    public void setProductAtShelf(int shelf, int productid, int count) throws RuntimeException {
+
+    }
+
+
+    //---------------
+    private boolean checkShelfNumber(int shelf) {
+        return shelf >= 0 && shelf < nShelves;
+    }
+
 }
--- src/test/java/tdd/vendingMachine/Domain/MockRepos.java
+++ src/test/java/tdd/vendingMachine/Domain/MockRepos.java
@@ -1,5 +1,6 @@
 package tdd.vendingMachine.Domain;
 
+
 /**
  * Filling repositories with some data
  */
@@ -8,5 +9,16 @@ public class MockRepos {
         repo.save(new Product("Cola", 10));
         repo.save(new Product("Sprite", 11));
         repo.save(new Product("Snack", 8));
+    }
+
+    //assumes pid={1,2,3} are valid; and srepo==null
+    public static StorageRepo createFillStorageRepo() {
+        StorageRepo srepo = new StorageRepo(5, 20);
+        srepo.setProductAtShelf(0, 1, 5);
+        srepo.setProductAtShelf(1, 1, 1);
+        srepo.setProductAtShelf(2, 1, 0);
+        srepo.setProductAtShelf(3, 2, 5);
+        srepo.setProductAtShelf(4, 3, 5);
+        return srepo;
     }
 }
