3458c9f0376595ec24dd49285266b4198f26192d
pmarecki <pmarecki.pro@gmail.com>
2016-02-18 08:11:21 +0100

    Packaging configured

--- src/main/java/tdd/vendingMachine/Starter.java
+++
@@ -1,9 +0,0 @@
-package tdd.vendingMachine;
-
-/**
- * Some start-point.
- * Will need to instantiate all repos and services, provide them to the client/admin app, and
- * pass control to these view-objects.
- */
-public class Starter {
-}
