7667b863d15f3eabe5b7270a60998e5731712445
Łukasz Gajowy <lukasz.gajowy@gmail.com>
2016-02-28 01:46:40 +0100

    If we select shelf number, display shows product price.

--- src/main/java/tdd/vendingMachine/domain/VendingMachine.java
+++ src/main/java/tdd/vendingMachine/domain/VendingMachine.java
@@ -7,8 +7,16 @@ public class VendingMachine {
 
     private Display display;
 
-    public VendingMachine(Display display) {
+    private String[] pricesPerShelves;
+
+    public VendingMachine(Display display, String[] pricesPerShelves) {
         this.display = display;
+        this.pricesPerShelves = pricesPerShelves;
         this.display.displayMessage("Welcome! Please choose product:");
+    }
+
+    public void acceptChoice(int shelfNumber) {
+        display.displayMessage(shelfNumber == 0 || shelfNumber > pricesPerShelves.length ?
+            "Invalid shelf choice. Please try again." : "Price: " + pricesPerShelves[shelfNumber - 1]);
     }
 }
--- src/test/java/tdd/vendingMachine/domain/VendingMachineTest.java
+++ src/test/java/tdd/vendingMachine/domain/VendingMachineTest.java
@@ -1,16 +1,22 @@
 package tdd.vendingMachine.domain;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.runners.MockitoJUnitRunner;
 import tdd.vendingMachine.external_interface.Display;
 
+import static org.mockito.Matchers.startsWith;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.only;
 
-@RunWith(MockitoJUnitRunner.class)
+
+@RunWith(JUnitParamsRunner.class)
 public class VendingMachineTest {
 
     @InjectMocks
@@ -19,8 +25,38 @@ public class VendingMachineTest {
     @Mock
     private Display displayMock;
 
+    @Before
+    public void setUp() throws Exception {
+        displayMock = mock(Display.class);
+        String[] pricesPerShelves = {"1.50", "3.00"};
+        machine = new VendingMachine(displayMock, pricesPerShelves);
+    }
+
     @Test
     public void should_display_welcome_message() throws Exception {
         Mockito.verify(displayMock, only()).displayMessage("Welcome! Please choose product:");
+    }
+
+    @Test
+    @Parameters({
+        "1, 1.50",
+        "2, 3.00"
+    })
+    public void should_show_product_price_if_shelf_is_selected(int shelfChoice, String priceValue) throws Exception {
+        machine.acceptChoice(shelfChoice);
+
+        Mockito.verify(displayMock).displayMessage("Price: " + priceValue);
+    }
+
+    @Test
+    @Parameters({
+        "0",
+        "3"
+    })
+    public void should_inform_about_invalid_choice(int shelfChoice) throws Exception {
+        machine.acceptChoice(shelfChoice);
+
+        Mockito.verify(displayMock).displayMessage("Invalid shelf choice. Please try again.");
+        Mockito.verify(displayMock, never()).displayMessage(startsWith("Price"));
     }
 }
