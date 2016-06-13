a0bfe521701643147bbaffe78f4ba0fed8eb2d15
Artem Korobko <artem.korobko@gmail.com>
2016-06-13 10:32:37 +0300

    Vending machine now can be configured in terms of allowed denominations

--- src/main/java/tdd/vendingMachine/VendingMachine.java
+++ src/main/java/tdd/vendingMachine/VendingMachine.java
@@ -1,5 +1,21 @@
 package tdd.vendingMachine;
 
+import tdd.vendingMachine.core.CurrencyUnit;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 public class VendingMachine {
 
+    private final Set<CurrencyUnit> allowedDenominations = new HashSet<>();
+
+    public VendingMachine addAllowedDenomination(CurrencyUnit currencyUnit) {
+        allowedDenominations.add(currencyUnit);
+        return this;
+    }
+
+    public Set<CurrencyUnit> getAllowedDenominations() {
+        return Collections.unmodifiableSet(allowedDenominations);
+    }
 }
--- src/test/java/tdd/vendingMachine/VendingMachineTest.java
+++ src/test/java/tdd/vendingMachine/VendingMachineTest.java
@@ -1,12 +1,18 @@
 package tdd.vendingMachine;
 
-import org.assertj.core.api.Assertions;
 import org.junit.Test;
+import tdd.vendingMachine.core.CurrencyUnit;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class VendingMachineTest {
 
     @Test
-    public void just_a_stupid_passing_test_to_ensure_that_tests_are_run() {
-        Assertions.assertThat(new VendingMachine()).isNotNull();
+    public void vending_machine_should_configure_allowed_denominations() {
+        VendingMachine vendingMachine = new VendingMachine();
+        vendingMachine.addAllowedDenomination(CurrencyUnit.valueOf("2"));
+        vendingMachine.addAllowedDenomination(CurrencyUnit.valueOf("5"));
+
+        assertThat(vendingMachine.getAllowedDenominations().size()).isEqualTo(2);
     }
 }
