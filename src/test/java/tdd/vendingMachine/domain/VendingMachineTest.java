44c216ba6b13d28a9d6fb21e125b98e1e60d3a6d
Grzegorz Budzyn <gbudzyn@gmail.com>
2016-11-12 10:49:43 +0100

    VMK-10 refining end-to-end tests

--- src/test/java/tdd/vendingMachine/domain/VendingMachineTest.java
+++ src/test/java/tdd/vendingMachine/domain/VendingMachineTest.java
@@ -1,16 +1,24 @@
 package tdd.vendingMachine.domain;
 
 import org.junit.Test;
+import tdd.vendingMachine.domain.display.Messages;
 import tdd.vendingMachine.domain.money.Coins;
+import tdd.vendingMachine.domain.product.Products;
 import tdd.vendingMachine.domain.state.States;
 
 import static org.junit.Assert.assertEquals;
 
 public class VendingMachineTest {
 
+    private static int BASE_MONEY_AMOUNT = 100;
+    private static int BASE_COCA_COLA_0_33_COUNT = 10;
+
     @Test
     public void should_set_up_new_vending_machine_in_base_state() {
-        assertEquals(States.BASE, new VendingMachine().getState());
+        VendingMachine machine = new VendingMachine();
+
+        assertEquals(States.BASE, machine.getState());
+        assertEquals(Messages.IDLE.getMessage(), machine.getDisplay().getMessage());
     }
 
     @Test
@@ -19,7 +27,13 @@ public class VendingMachineTest {
         machine.pressTraySelectionButton(0);
 
         assertEquals(States.TRAY_SELECTED, new VendingMachine().getState());
-        assertEquals("Price: 20", machine.getDisplay().getMessage());
+        assertEquals(
+            String.format(
+                Messages.PRODUCT_SELECTED.getMessage(),
+                Products.COCA_COLA_0_33.name(),
+                Products.COCA_COLA_0_33.getPrice()
+            ), machine.getDisplay().getMessage()
+        );
     }
 
     @Test
@@ -29,53 +43,65 @@ public class VendingMachineTest {
         machine.insertCoin(Coins.COIN_0_1);
 
         assertEquals(States.COIN_INSERTED, new VendingMachine().getState());
+        assertEquals(
+            String.format(
+                Messages.COINS_INSERTED.getMessage(),
+                0.1,
+                (Products.COCA_COLA_0_33.getPrice() / 10 - 0.1)),
+            machine.getDisplay().getMessage()
+        );
     }
 
     @Test
-    //TODO - add inserted money return check if possible
     public void should_return_to_base_state_on_cancel_pressed() {
         VendingMachine machine = new VendingMachine();
-        machine.pressTraySelectionButton(1);
+        machine.pressTraySelectionButton(0);
         machine.insertCoin(Coins.COIN_0_1);
         machine.pressCancelButton();
 
         assertEquals(States.BASE, new VendingMachine().getState());
+        assertEquals(Messages.IDLE.getMessage(), machine.getDisplay().getMessage());
     }
 
     @Test
-    //TODO - add tray inserted check if needed? possible exposing internal components
     public void should_not_allow_to_change_product_when_money_inserted() {
         VendingMachine machine = new VendingMachine();
-        machine.pressTraySelectionButton(1);
+        machine.pressTraySelectionButton(0);
         machine.insertCoin(Coins.COIN_0_1);
-        machine.pressTraySelectionButton(2);
+        machine.pressTraySelectionButton(1);
 
         assertEquals(States.COIN_INSERTED, new VendingMachine().getState());
+        assertEquals(Messages.IDLE.getMessage(), machine.getDisplay().getMessage());
+        assertEquals(BASE_MONEY_AMOUNT, machine.getMoneyBox().getTotalAmount());
     }
 
     @Test
-    //TODO - add product dispensed check
     public void should_dispense_product_and_be_back_in_base_state_when_correct_amount_of_money_inserted() {
         VendingMachine machine = new VendingMachine();
-        machine.pressTraySelectionButton(1);
+        machine.pressTraySelectionButton(0);
         machine.insertCoin(Coins.COIN_0_1);
         machine.insertCoin(Coins.COIN_0_1);
         machine.insertCoin(Coins.COIN_0_1);
         machine.insertCoin(Coins.COIN_0_1);
 
         assertEquals(States.BASE, new VendingMachine().getState());
+        assertEquals(Messages.IDLE.getMessage(), machine.getDisplay().getMessage());
+        assertEquals(BASE_MONEY_AMOUNT - Products.COCA_COLA_0_5.getPrice(), machine.getMoneyBox().getTotalAmount());
+        assertEquals(BASE_COCA_COLA_0_33_COUNT - 1, machine.getProductBox().getTray(0).getProductCount());
     }
 
     @Test
-    //TODO - add product not dispensed and money returned check
     public void should_not_dispense_product_give_back_money_and_get_back_to_base_state_when_unable_to_give_change() {
         VendingMachine machine = new VendingMachine();
-        machine.pressTraySelectionButton(1);
+        machine.pressTraySelectionButton(0);
         machine.insertCoin(Coins.COIN_0_1);
         machine.insertCoin(Coins.COIN_0_1);
         machine.insertCoin(Coins.COIN_0_1);
         machine.insertCoin(Coins.COIN_0_1);
 
         assertEquals(States.BASE, new VendingMachine().getState());
+        assertEquals(Messages.IDLE.getMessage(), machine.getDisplay().getMessage());
+        assertEquals(BASE_MONEY_AMOUNT, machine.getMoneyBox().getTotalAmount());
+        assertEquals(BASE_COCA_COLA_0_33_COUNT, machine.getProductBox().getTray(0).getProductCount());
     }
 }
