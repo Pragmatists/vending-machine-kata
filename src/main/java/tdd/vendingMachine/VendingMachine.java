a8c0f3e2237530333cd9d5ec64ae7735b9b92399
asole83 <asolevall@gmail.com>
2017-01-29 19:16:43 +0100

    Definition of classes
    Definition of the classes and the Interfaces

--- src/main/java/tdd/vendingMachine/VendingMachine.java
+++ src/main/java/tdd/vendingMachine/VendingMachine.java
@@ -1,5 +1,22 @@
-package tdd.vendingMachine;
+package main.java.tdd.vendingMachine;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class VendingMachine {
-
+	//All the sections of the Vending Machine
+	private List<ISection> sections;
+	
+	//The text shown in the display of the Vending Machine
+	private String displayText;
+	
+	//Money the user has already introduced in the machine
+	private float currentMoney;
+	
+	
+	//Money accumulated in the machine, it's useful to know if we can give change or not
+	private float cumulativeMoney; 
+	
 }
+
+
