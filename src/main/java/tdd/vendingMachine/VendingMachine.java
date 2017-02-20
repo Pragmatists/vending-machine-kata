cb8d826ae2dd97e32c8348b8a038eb57ee59ebde
Agustin Cabra <Agustin.Cabra@nielsen.com>
2017-02-20 16:41:58 +0100

    S01_06 Implementing the Vending machine as observable and as member of state pattern, defining basic states SOLD_OUT_STATE when the machine is empty, and NO_CREDIT_STATE that resembles no coins have been inserted to the machine

--- src/main/java/tdd/vendingMachine/VendingMachine.java
+++ src/main/java/tdd/vendingMachine/VendingMachine.java
@@ -1,5 +1,82 @@
 package tdd.vendingMachine;
 
-public class VendingMachine {
+import com.google.common.util.concurrent.AtomicDouble;
+import org.apache.log4j.Logger;
+import tdd.vendingMachine.domain.Coin;
+import tdd.vendingMachine.domain.Product;
+import tdd.vendingMachine.domain.Shelf;
+import tdd.vendingMachine.state.NoCreditState;
+import tdd.vendingMachine.state.SoldOutState;
+import tdd.vendingMachine.state.State;
 
+import java.util.Map;
+import java.util.Observable;
+
+public class VendingMachine extends Observable implements State {
+
+    private static final Logger logger = Logger.getLogger(VendingMachine.class);
+
+    //States
+    public final SoldOutState soldOutState;
+    public final NoCreditState noCreditState;
+
+    private Product selectedProduct;
+    private AtomicDouble credit;
+    private Map<String, Shelf<Product>> productShelves;
+    private final Map<Coin, Shelf<Coin>> coinShelves;
+    private Map<String, String> shelfLabelsToIds;
+
+    private State currentState;
+
+    public VendingMachine(Map<String, Shelf<Product>> productShelves, Map<Coin, Shelf<Coin>> coinShelves) {
+        this.productShelves = productShelves;
+        this.coinShelves = coinShelves;
+        this.credit = new AtomicDouble(0.0);
+        this.selectedProduct = null;
+
+        this.soldOutState = new SoldOutState(this);
+        this.noCreditState = new NoCreditState(this);
+
+        if (productShelves == null || productShelves.size() == 0) {
+            this.currentState = soldOutState;
+        } else {
+            int availableProducts = productShelves.values().stream().map(Shelf::getItemCount).reduce(0, (a, b) -> a + b);
+            this.currentState = availableProducts > 0 ? noCreditState : soldOutState;
+        }
+    }
+
+    @Override
+    public void insertCoin(Coin money) {
+        currentState.insertCoin(money);
+    }
+
+    @Override
+    public void selectProduct(String shelfLabel) {
+        currentState.selectProduct(shelfLabel);
+    }
+
+    @Override
+    public void cancel() {
+        currentState.cancel();
+    }
+
+    /**
+     * Returns the available credit on the vending machine
+     * @return double must be greater or equal to zero.
+     */
+    public double getCredit() {
+        return credit.get();
+    }
+
+    /**
+     * Provision cash to vending machine if space is available
+     * @param coin the coin shelf to provision
+     * @param amount the amount of coins
+     */
+    public void provisionCoinShelf(Coin coin, int amount) {
+    }
+
+    public State getCurrentState() {
+        return currentState;
+    }
 }
--- src/test/java/tdd/vendingMachine/VendingMachineTest.java
+++ src/test/java/tdd/vendingMachine/VendingMachineTest.java
@@ -1,12 +1,33 @@
 package tdd.vendingMachine;
 
-import org.assertj.core.api.Assertions;
+import org.junit.Assert;
 import org.junit.Test;
+import tdd.vendingMachine.domain.Product;
+import tdd.vendingMachine.domain.Shelf;
+import tdd.vendingMachine.domain.ShelfProductFactory;
+import tdd.vendingMachine.state.NoCreditState;
+import tdd.vendingMachine.state.SoldOutState;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 public class VendingMachineTest {
 
     @Test
-    public void just_a_stupid_passing_test_to_ensure_that_tests_are_run() {
-        Assertions.assertThat(new VendingMachine()).isNotNull();
+    public void should_be_sold_out_state_for_empty_created_vending_machine() {
+        VendingMachine emptyVendingMachine = new VendingMachine(Collections.emptyMap(), Collections.emptyMap());
+        Assert.assertTrue(emptyVendingMachine.getCurrentState() instanceof SoldOutState);
+    }
+
+    @Test
+    public void should_be_no_credit_state_for_non_empty_created_vending_machine() {
+        VendingMachine nonEmptyVendingMachine = new VendingMachine(buildShelvesOneItem(), new HashMap<>());
+        Assert.assertTrue(nonEmptyVendingMachine.getCurrentState() instanceof NoCreditState);
+    }
+
+    private Map<String, Shelf<Product>> buildShelvesOneItem() {
+        Shelf<Product> shelf = ShelfProductFactory.buildShelf("shelf1", new Product(1.99, "cola"), 10, 1);
+        return new HashMap<String, Shelf<Product>>(){{put(shelf.id, shelf);}};
     }
 }
