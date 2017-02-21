d0492cbf35c02bc15749dc4f3c1dc305ca2e05f4
Agustin Cabra <Agustin.Cabra@nielsen.com>
2017-02-21 14:03:00 +0100

    S01_06 The vending machine now complies to the new use of shelves with integer ids and provides null checking on constructor

--- src/main/java/tdd/vendingMachine/VendingMachine.java
+++ src/main/java/tdd/vendingMachine/VendingMachine.java
@@ -1,14 +1,17 @@
 package tdd.vendingMachine;
 
 import com.google.common.util.concurrent.AtomicDouble;
+import lombok.NonNull;
 import org.apache.log4j.Logger;
 import tdd.vendingMachine.domain.Coin;
 import tdd.vendingMachine.domain.Product;
 import tdd.vendingMachine.domain.Shelf;
+import tdd.vendingMachine.domain.VendingMachineConfiguration;
 import tdd.vendingMachine.state.NoCreditState;
 import tdd.vendingMachine.state.SoldOutState;
 import tdd.vendingMachine.state.State;
 
+import java.util.InputMismatchException;
 import java.util.Map;
 import java.util.Observable;
 
@@ -16,28 +19,32 @@ public class VendingMachine extends Observable implements State {
 
     private static final Logger logger = Logger.getLogger(VendingMachine.class);
 
+    public static final VendingMachineConfiguration VENDING_MACHINE_CONFIGURATION = new VendingMachineConfiguration();
+
     //States
     public final SoldOutState soldOutState;
     public final NoCreditState noCreditState;
 
     private Product selectedProduct;
     private AtomicDouble credit;
-    private Map<String, Shelf<Product>> productShelves;
-    private final Map<Coin, Shelf<Coin>> coinShelves;
-    private Map<String, String> shelfLabelsToIds;
+    private Map<Integer, Shelf<Product>> productShelves;
+    private Map<String, Integer> productShelfLabelsToIds;
 
     private State currentState;
 
-    public VendingMachine(Map<String, Shelf<Product>> productShelves, Map<Coin, Shelf<Coin>> coinShelves) {
+    public VendingMachine(@NonNull Map<Integer, Shelf<Product>> productShelves, @NonNull Map<Coin, Shelf<Coin>> coinShelves) {
+        if(productShelves.size() > VENDING_MACHINE_CONFIGURATION.getShelfCount()) {
+            throw new InputMismatchException(String.format("Unable to create Vending Machine configuration mismatch for shelves expected %d given %d ",
+                VENDING_MACHINE_CONFIGURATION.getShelfCount(), productShelves.size()));
+        }
         this.productShelves = productShelves;
-        this.coinShelves = coinShelves;
         this.credit = new AtomicDouble(0.0);
         this.selectedProduct = null;
 
         this.soldOutState = new SoldOutState(this);
         this.noCreditState = new NoCreditState(this);
 
-        if (productShelves == null || productShelves.size() == 0) {
+        if (productShelves.size() == 0) {
             this.currentState = soldOutState;
         } else {
             int availableProducts = productShelves.values().stream().map(Shelf::getItemCount).reduce(0, (a, b) -> a + b);
