d1cd0898ebb303402e3f6fc2bef6c8a04a418829
Agustin Cabra <cabra.agustin@gmail.com>
2017-02-26 14:20:20 +0100

    S04_03 adding behaviour for state NoCreditSelectedProduct for action selectShelf

--- src/main/java/tdd/vendingMachine/state/NoCreditSelectedProductState.java
+++ src/main/java/tdd/vendingMachine/state/NoCreditSelectedProductState.java
@@ -1,7 +1,13 @@
 package tdd.vendingMachine.state;
 
+import org.apache.log4j.Logger;
 import tdd.vendingMachine.VendingMachine;
 import tdd.vendingMachine.domain.Coin;
+import tdd.vendingMachine.domain.exception.CashDispenserFullException;
+import tdd.vendingMachine.domain.exception.ShelfEmptyNotAvailableForSelectionException;
+import tdd.vendingMachine.view.VendingMachineMessages;
+
+import java.util.NoSuchElementException;
 
 /**
  * @author Agustin Cabra on 2/21/2017.
@@ -9,6 +15,7 @@ import tdd.vendingMachine.domain.Coin;
  */
 public class NoCreditSelectedProductState implements State {
 
+    private static final Logger logger = Logger.getLogger(NoCreditSelectedProductState.class);
     public static final String label = "NO CREDIT SELECTED PRODUCT";
     final VendingMachine vendingMachine;
 
@@ -18,12 +25,34 @@ public class NoCreditSelectedProductState implements State {
 
     @Override
     public void insertCoin(Coin coin) {
-
+        try {
+            vendingMachine.addCoinToCredit(coin);
+        } catch (CashDispenserFullException cashDispenserFullException) {
+            logger.error(cashDispenserFullException);
+            String message = String.format("%s %s: %s", coin.label,
+                cashDispenserFullException.getMessage(),
+                VendingMachineMessages.provideCashToDisplay(this.vendingMachine.getCredit()));
+            this.vendingMachine.showMessageOnDisplay(message);
+        }
     }
 
     @Override
     public void selectShelfNumber(int shelfNumber) {
-
+        try {
+            vendingMachine.selectProductGivenShelfNumber(shelfNumber);
+            vendingMachine.displayProductPrice(shelfNumber);
+        } catch (NoSuchElementException nse) {
+            logger.error(nse);
+            vendingMachine.showMessageOnDisplay(
+                VendingMachineMessages.buildWarningMessageWithSubject(VendingMachineMessages.SHELF_NUMBER_NOT_AVAILABLE.label,
+                    shelfNumber, false));
+        } catch (ShelfEmptyNotAvailableForSelectionException e) {
+            logger.error(e);
+            this.vendingMachine.showMessageOnDisplay(VendingMachineMessages.buildWarningMessageWithSubject(
+                VendingMachineMessages.UNABLE_TO_SELECT_EMPTY_SHELF.label,
+                e.getShelfNumber(), false)
+            );
+        }
     }
 
     @Override
