52bcadccd391533774fab3c8a2ed4832c928b009
sukhomud <suhomud@gmail.com>
2017-07-07 16:14:09 +0200

    Memory implementation

--- src/main/java/tdd/vendingMachine/KingstonMemoryCard.java
+++ src/main/java/tdd/vendingMachine/KingstonMemoryCard.java
@@ -2,6 +2,7 @@ package tdd.vendingMachine;
 
 import tdd.vendingMachine.model.Product;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -9,44 +10,54 @@ import java.util.List;
  */
 public class KingstonMemoryCard implements Memory {
 
+    private Product product;
+    private int index = 0;
+    private List<Double> money = new ArrayList<>();
+
     @Override
     public int productIndex() {
-        return 0;
+        return index;
     }
 
     @Override
     public List<Double> insertedMoney() {
-        return null;
+        return new ArrayList<>(money);
     }
 
     @Override
     public double price() {
-        return 0;
+        if (product == null) {
+            return 0;
+        }
+        return product.price();
     }
 
     @Override
     public void clear() {
-
+        product = null;
+        index = 0;
+        money.clear();
     }
 
     @Override
     public void remember(Product product, int index) {
-
+        this.product = product;
+        this.index = index;
     }
 
     @Override
     public void remember(double money) {
-
+        this.money.add(money);
     }
 
     @Override
     public boolean hasInsertedMoney() {
-        return false;
+        return money.size() > 0;
     }
 
     @Override
     public boolean hasSelectedProduct() {
-        return false;
+        return product != null;
     }
 
 }
